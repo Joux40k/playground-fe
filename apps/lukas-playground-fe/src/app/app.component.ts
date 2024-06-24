5bb984a19761a64197f664844dfa869703c86c90
lukas <lukas0bornscheuer@gmail.com>
2024-06-24 16:29:20 +0200

    init

--- apps/lukas-playground-fe/src/app/app.component.ts
+++ apps/lukas-playground-fe/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { NewsComponent } from './components/news/news.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, NavigationBarComponent],
+  imports: [RouterModule, NavigationBarComponent, LogoComponent, NewsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/lukas-playground-fe/src/app/components/navigation-bar/navigation-bar.component.ts
+++ apps/lukas-playground-fe/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoComponent } from '../logo/logo.component';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'nav-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LogoComponent, MatButton],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss',
 })
